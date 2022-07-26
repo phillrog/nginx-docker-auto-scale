8e5f937511965f11f6375c29e022ee6e05b4e2ae
phillrog <phillrog@hotmail.com>
2022-07-26 00:13:40 -0300

    COnfiguração variáveis do docker para a aplicação

--- portal/src/app/app.component.ts
+++ portal/src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import config from '../assets/config/config.json';
+
+type NewType = Observable<any>;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portal';
+
+  title = `${config.app.ip} - ${config.app.name}`;
+  constructor(private http: HttpClient) {
+    this.getJSON().subscribe((data: any) => {
+      this.title = data.app.name;
+    });
+  }
+  public getJSON(): NewType {
+    return this.http.get('./assets/config/config.json');
+  }
 }
